5a8a5cbf371e0a32b7c3e7b79c53490d695c69a0
sacgupta.ext <sacgupta.ext@deloitte.com>
2021-01-27 01:56:11 +0530

    end points

---
+++ src/core/apis/endPoints.ts
@@ -0,0 +1,7 @@
+export const endPoints = {
+  TOP_STORIES: "/topstories.json",
+  ASK_STORIES: "/askstories.json",
+  SHOW_STORIES: "/showstories.json",
+  JOBS_STORIES: "/jobstories.json",
+  GET_PROFILE: "/user",
+};
