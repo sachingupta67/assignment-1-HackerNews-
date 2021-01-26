8e978741d74a1260fadd9fb09e8d36be22d096f9
sacgupta.ext <sacgupta.ext@deloitte.com>
2021-01-27 01:58:54 +0530

    styles object

--- src/pages/dashboard/styles.ts
+++ src/pages/dashboard/styles.ts
@@ -1,13 +1,15 @@
+import { colors } from "../../theme";
+const { BLACK, GRAY } = colors;
 export const styles = {
   container: {
     width: "100%",
-    borderTop: "1px solid black",
+    borderTop: `1px solid ${BLACK}`,
   },
   title: {
     paddingLeft: "5%",
     paddingRight: "5%",
     paddingBottom: "1%",
-    borderBottom: "1px solid black",
+    borderBottom: `1px solid ${BLACK}`,
   },
   searchWrapper: {
     justifyContent: "space-around",
@@ -19,4 +21,17 @@ export const styles = {
   radio: {
     width: "30%",
   },
+  profileContainer: {
+    paddingLeft: "5%",
+    paddingRight: "5%",
+  },
+  profileDetails: {
+    color: GRAY,
+  },
+  listContainer: {
+    paddingRight: "3%",
+    paddingLeft: "3%",
+  },
+  listTitle: { marginBottom: 5 },
+  listSubTitle: { color: GRAY, margin: 0 },
 };
