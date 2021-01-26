70f16d13b535651d93457e2d58d3dd45a79921d4
sacgupta.ext <sacgupta.ext@deloitte.com>
2021-01-26 18:06:27 +0530

    dashboard styles

---
+++ src/pages/dashboard/styles.ts
@@ -0,0 +1,22 @@
+export const styles = {
+  container: {
+    width: "100%",
+    borderTop: "1px solid black",
+  },
+  title: {
+    paddingLeft: "5%",
+    paddingRight: "5%",
+    paddingBottom: "1%",
+    borderBottom: "1px solid black",
+  },
+  searchWrapper: {
+    justifyContent: "space-around",
+    display: "flex",
+  },
+  search: {
+    width: "60%",
+  },
+  radio: {
+    width: "30%",
+  },
+};
