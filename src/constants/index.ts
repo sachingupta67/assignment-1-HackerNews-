8204b1f5b58574f1f31594e3531d1f7085ce778b
sacgupta.ext <sacgupta.ext@deloitte.com>
2021-01-26 18:05:40 +0530

    constants

---
+++ src/constants/index.ts
@@ -0,0 +1,20 @@
+export const constanst = {
+  stories: [
+    {
+      id: 1,
+      label: "Top",
+    },
+    {
+      id: 2,
+      label: "Ask",
+    },
+    {
+      id: 3,
+      label: "Show",
+    },
+    {
+      id: 4,
+      label: "Jobs",
+    },
+  ],
+};
