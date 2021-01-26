a640c8aba2fe2b41b07c45fc578704576c3bd524
sacgupta.ext <sacgupta.ext@deloitte.com>
2021-01-27 01:58:14 +0530

    dashboard screen

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import SearchHeader from "./searchHeader";
+import ResultList from "./resultList";
+import { useSelector } from "react-redux";
 import { styles } from "./styles";
 import { translate as ts } from "../../core/i18n/translate";
-import SearchHeader from "./searchHeader";
+import { commonSelector } from "../../core/redux/selectors";
+import Profile from "./profile";
 const Dashboard: React.FC = () => {
+  const { result } = useSelector(commonSelector);
+  const [showResult, setShowResult] = useState(true);
   return (
     <div style={styles.container}>
       <h1 style={styles.title}>{ts("HEADER_TITLE")}</h1>
-      <SearchHeader />
+      {showResult ? (
+        <SearchHeader />
+      ) : (
+        <Profile toggleHandler={setShowResult} />
+      )}
+      {showResult ? (
+        <ResultList data={result} toggleHandler={setShowResult} />
+      ) : null}
     </div>
   );
 };
