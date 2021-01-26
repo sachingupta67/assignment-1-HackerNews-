3a3ee2b1f7e3d1717bb75437652e497853b2fb46
sacgupta.ext <sacgupta.ext@deloitte.com>
2021-01-27 01:58:42 +0530

    search header

--- src/pages/dashboard/searchHeader.tsx
+++ src/pages/dashboard/searchHeader.tsx
@@ -1,12 +1,44 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import TextInput from "./../../components/textInput";
 import { styles } from "./styles";
 import { translate as ts } from "../../core/i18n/translate";
 import { constanst } from "../../constants";
+import {
+  getShowNews,
+  getAskNews,
+  getTopNews,
+  getJobNews,
+  seachFilterAction,
+} from "../../core/redux/actions";
+import { commonSelector } from "../../core/redux/selectors";
+import { useDispatch, useSelector } from "react-redux";
 const RADIO_DATA = constanst.stories;
 const SearchHeader: React.FC = () => {
   const [search, setSearch] = useState("");
   const [radioVal, setRadioVal] = useState(RADIO_DATA[0]);
+  const { result } = useSelector(commonSelector);
+  const dispatch = useDispatch();
+  const newsHandler = useCallback(() => {
+    const { id } = radioVal;
+    if (id === 1) {
+      dispatch(getTopNews());
+    }
+    if (id === 2) {
+      dispatch(getAskNews());
+    }
+    if (id === 3) {
+      dispatch(getShowNews());
+    }
+    if (id === 4) {
+      dispatch(getJobNews());
+    }
+  }, [radioVal.id]);
+  useEffect(() => {
+    dispatch(getTopNews());
+  }, []);
+  useEffect(() => {
+    newsHandler();
+  }, [radioVal.id]);
   const radioJSX = RADIO_DATA.map((item) => (
     <TextInput
       key={item.id}
@@ -19,16 +51,24 @@ const SearchHeader: React.FC = () => {
     />
   ));
 
+  const searchHandler = (val: string) => {
+    setSearch(val);
+    // let data = result.filter((item: any) =>
+    //   item.title.toLowerCase().match(val.toLowerCase())
+    // );
+    // dispatch(seachFilterAction(data));
+  };
+
   return (
     <div style={styles.searchWrapper}>
       <div style={styles.search}>
         <TextInput
-          onChange={(x: any, y: any) => {
-            // setVal(y);
+          onChange={(_name: any, value: any) => {
+            searchHandler(value);
           }}
-          name={"text"}
+          name={"search"}
           type="text"
-          value={""}
+          value={search}
           placeholder={ts("SEARCH_PLACEHOLDER")}
         />
       </div>
