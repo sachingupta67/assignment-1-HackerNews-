fb06de5c2643f947b6cfee6ea714201309e91d29
sacgupta.ext <sacgupta.ext@deloitte.com>
2021-01-27 01:57:21 +0530

    redux constants

---
+++ src/core/redux/contants/index.ts
@@ -0,0 +1,16 @@
+const LOADING = "Loading";
+const SHOW_NEWS = "SHOW_NEWS";
+const ASK_NEWS = "ASK_NEWS";
+const TOP_NEWS = "TOP_NEWS";
+const JOB_NEWS = "JOB_NEWS";
+const RESULT = "RESULT";
+const GET_PROFILE_INFO = "GET_PROFILE_INFO";
+export const constants = {
+  LOADING,
+  SHOW_NEWS,
+  ASK_NEWS,
+  TOP_NEWS,
+  JOB_NEWS,
+  RESULT,
+  GET_PROFILE_INFO,
+};
