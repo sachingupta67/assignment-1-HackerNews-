d11580f1e3ef77073f23ea42c7dad5746590b8ad
sacgupta.ext <sacgupta.ext@deloitte.com>
2021-01-27 01:56:49 +0530

    lables added to translate file

--- src/core/i18n/messages/en.ts
+++ src/core/i18n/messages/en.ts
@@ -4,6 +4,14 @@ export default {
     translation: {
       HEADER_TITLE: "HackNews LIVE",
       SEARCH_PLACEHOLDER: "Search by title, user ...",
+      USER_PROFILE: "User Profile",
+      USER: "user",
+      CREATED: "created",
+      ABOUT: "about",
+      BACK: "Back",
+      LOADING: "Loading...",
+      KARMA: "Karma",
+      NO_DATA: "Data not found",
     },
   },
 };
